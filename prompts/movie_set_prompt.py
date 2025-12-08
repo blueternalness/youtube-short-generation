9914ff6662a1493e16093692941cf5deb54aa64c
김우영 <wooyoung.kim365@gmail.com>
2025-12-08 01:20:05 -0800

    initial commit
    Signed-off-by: 김우영 <wooyoung.kim365@gmail.com>

---
+++ prompts/movie_set_prompt.py
@@ -0,0 +1,17 @@
+
+# Characters in Movie set
+
+# Flow
+
+# step1 Image generation with API
+# Create humans cosplaying attached character based on the character in attached image.
+
+# Step2 Video generation with API
+"""
+I'm going to create a video that shows a movie set where cartoon characters are being brought to life in live action. Follow my steps and instructions and create a well made video as long as possible.
+
+Step1: Create humans cosplaying attached character based on the character in attached image.
+Step2: Don't put any sounds in the video.
+Step3: In the video, the humans should interact with film crew or act in scenes with various background
+Step4: There should be a series of different scenes.
+"""
