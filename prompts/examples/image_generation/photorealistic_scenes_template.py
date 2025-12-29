276529f8ee4aef4f846d9bdd49fe34a0111aabb1
김우영 <wooyoung.kim365@gmail.com>
2025-12-29 00:45:25 -0800

    feat: add image generation template from google
    Signed-off-by: 김우영 <wooyoung.kim365@gmail.com>

---
+++ prompts/examples/image_generation/photorealistic_scenes_template.py
@@ -0,0 +1,7 @@
+"""
+https://developers.googleblog.com/en/how-to-prompt-gemini-2-5-flash-image-generation-for-the-best-results/
+"""
+
+PHOTOREALISTIC_SCENES_TEMPLATE = """
+A photorealistic [shot type] of [subject], [action or expression], set in [environment]. The scene is illuminated by [lighting description], creating a [mood] atmosphere. Captured with a [camera/lens details], emphasizing [key textures and details]. The image should be in a [aspect ratio] format.
+"""
---
+++ prompts/examples/image_generation/product_mockups_template.py
@@ -0,0 +1,7 @@
+"""
+https://developers.googleblog.com/en/how-to-prompt-gemini-2-5-flash-image-generation-for-the-best-results/
+"""
+
+PRODUCT_MOKCKUPS_TEMPLATE = """
+A high-resolution, studio-lit product photograph of a [product description] on a [background surface/description]. The lighting is a [lighting setup, e.g., three-point softbox setup] to [lighting purpose]. The camera angle is a [angle type] to showcase [specific feature]. Ultra-realistic, with sharp focus on [key detail]. [Aspect ratio].
+"""
